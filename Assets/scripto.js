76dedd72cccf56b2bd88cab9b22ce0091bba9d3a
unbegat <109902145+unbegat@users.noreply.github.com>
2022-07-25 19:02:21 +0530

    Add files via upload

--- Assets/scripto.js
+++ Assets/scripto.js
@@ -2,3 +2,31 @@ function RandoInt() {
   ///the number in *() should be replaced with number of issues-1////
   return Math.floor(Math.random() * (2) ) + 1;
 }
+function randomSite(){
+  i = RandoInt();
+  location.href= i + ".htm";}   
+
+const Issue = ["#1.The Undersea Experience","#2. The Angel and the Owl"];
+
+window.onload = function(){
+  // Get the image
+  const image = new Image()
+  image.src = 'Comics/' + no +'.png'
+
+  // Wait Till the Image is loaded
+  image.onload = function() {
+    drawCanvas(image)
+  }
+
+  function drawCanvas(image) {
+    // Create canvas context
+    const canvas = document.getElementById('floornet')
+    const context = canvas.getContext("2d")
+
+    canvas.width = image.width 
+    canvas.height = image.height 
+
+    // Draw image to the canvas
+    context.drawImage(image, 0, 0) 
+  }
+}
